66f59b1d713858e5e02b76eb1a717ca1de273eae
Napomuceno <erjeffersonnapomuceno@gmail.com>
2023-02-05 20:17:22 -0300

    App locale, number and date formatting

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -9,6 +9,7 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
